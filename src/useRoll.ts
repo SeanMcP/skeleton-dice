a1bc1dd539083798f0525a5ce546776c9077161f
Sean McPherson <git@seanmcp.com>
2019-10-21 17:44:52 -0400

    Add return typing for useRoll
    Typescript was unable to infer correctly without explicit typing.

--- src/useRoll.ts
+++ src/useRoll.ts
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const useRoll = (sides = 6) => {
+const useRoll = (sides = 6): [number, () => void] => {
     const [value, setValue] = React.useState(sides)
 
     function roll() {
