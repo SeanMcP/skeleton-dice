1c601a57d8e83e5423386be4beae2febbbd87f5c
Sean McPherson <git@seanmcp.com>
2019-10-21 18:08:04 -0400

    Render Die based on query string

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import useQuery from './useQuery';
+import Die from './Die';
 import QueryLink from './QueryLink';
+import useQuery from './useQuery';
 
 const App: React.FC = () => {
     const query = useQuery()
-    console.log('query', query)
     return (
         <div className="App">
             <Switch>
                 <Route path="/">
+                    {query.d && query.d.map((die, i) => <Die key={`${die}-${i}`} sides={Number(die[0])} style={die[1]} />)}
                     <QueryLink query="d=6r">Add a red six-sided die</QueryLink>
                 </Route>
             </Switch>
